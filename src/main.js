903c832264f3c8c1c85669258010777ca1dbbfc0
Vitaliy <131911773+whyzzz20@users.noreply.github.com>
2024-02-01 18:23:12 +0100

    last commit

--- src/main.js
+++ src/main.js
@@ -1,86 +1,94 @@
-//Імпорти бібліотек
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-//Посилання на форму та галерею
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const BASE_URL= "https://pixabay.com/api/";
+const API_KEY = "41911500-2109ce3c8bb16633259977e96";
+
+const container = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
+const input= document.querySelector('.search-input');
+const startBtn= document.querySelector('.start-btn');
 const loader = document.querySelector('.loader');
-//Прослуховування "сабміту" форми
-form.addEventListener('submit', onFormSuccess);
-let counter = 0;
+
 loader.style.display = 'none';
+form.addEventListener('submit', handleSearch)
 
-//Отримання і відмальовка фото
-function onFormSuccess(e) {
-  e.preventDefault();
-  gallery.innerHTML = '';
+function handleSearch(event){
+  event.preventDefault();
   loader.style.display = 'block';
-
-  searchImage(e.target.elements.searchQuery.value);
-  e.target.elements.searchQuery.value = '';
-}
-
-//Функція запиту данних по пошуковому слову
-function searchImage(searchWord) {
-  fetch(
-    `https://pixabay.com/api/?key=41293253-42a55b268bdac57d89d3cc200&q=${searchWord}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(r => {
-      if (!r.ok) {
-        throw new Error(r.status);
-      }
-      return r.json();
-    })
-    .then(r => {
-      if (r.total === 0) {
-        iziToast.warning({
-          title: 'Wrong request',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
+  const inputValue = event.target.elements.query.value;
+  fetchImage(inputValue)
+  .then(data => {
+    loader.style.display = 'none';
+      if(!data.hits.length){
+          iziToast.error({
+              title: 'Error',
+              message:
+                'Sorry, there are no images matching your search query. Please try again!',
+            });
+      } 
+      container.innerHTML = "";
+      container.innerHTML = createMarkup(data.hits);
+      const refreshPage = new SimpleLightbox('.gallery a', {
+          captionsData: 'alt',
+          captionDelay: 250,
         });
-        return;
-      }
-      gallery.innerHTML = makeMarkup(r.hits);
-      counter += 1;
-      let lightbox = new SimpleLightbox('.gallery a');
-      lightbox.refresh();
-      loader.style.display = 'none';
-    })
-    .catch(error =>
-      iziToast.error({
-        title: 'Error',
-        message: `Oh, we have problem: ${error}`,
+        refreshPage.refresh();
+        form.reset();
+        
       })
-    );
+  .catch(onFetchError)
 }
 
-//Створення розмітки з масиву
 
-function makeMarkup(arr) {
+function fetchImage(name){
+  
+const url = `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=18`;
+
+    return fetch(url).then((resp)=> {
+        if(!resp.ok){
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+    })
+    
+}
+
+function createMarkup(arr) {
   return arr
     .map(
-      ({
-        webformatURL,
+      ({ webformatURL,
         largeImageURL,
         tags,
         likes,
         views,
         comments,
-        downloads,
-        id,
-      }) => `<a class="gallery__link" href="${largeImageURL}">
-  <div class="gallery-item" id="${id}">
-    <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy">
-    <div class="info">
-      <p class="info-item"><b>Likes</b>${likes}</p>
-      <p class="info-item"><b>Views</b>${views}</p>
-      <p class="info-item"><b>Comments</b>${comments}</p>
-      <p class="info-item"><b>Downloads</b>${downloads}</p>
-    </div>
-  </div>
-</a>`
+        downloads, }) => `
+  <li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${webformatURL}"
+      alt="${tags}"
+    />
+    <p class= "gallery-descr">• Likes: ${likes} • Views: ${views} • Comments: ${comments} •</span> Downloads:${downloads}</p>
+  </a>
+</li>
+  `
     )
     .join('');
+    
 }
+
+function onFetchError(error){
+  iziToast.error({
+    title: 'Error',
+    message:
+      'OOps... Plese try again',
+  });
+}
+    
